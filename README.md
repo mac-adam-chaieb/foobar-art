b72ef363f4d6ca910aa1483f4233538c13b2a613
mac-adam-chaieb <moe.chaieb@shopify.com>
2018-05-27 01:37:37 -0400

    Update README and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # foobar-covers
-A script to automatically generate covers for Foobar's SoundCloud page
+A script to automatically generate covers for Foobar's SoundCloud page.
+
+To get this running, simply do the following:
+```
+# Unless the covers directory already exists
+mkdir covers
+
+# This will generate a 100 Foobar covers in the covers directory
+ruby colorify.rb 100
+```
+
+This script depends on the `rmagick`, `chroma`, and `progressbar` gems.
--- colorify.rb
+++ colorify.rb
@@ -6,18 +6,22 @@ require 'progressbar'
 $number_of_covers = ARGV[0].to_i
 $source_path = 'foobar.png'
 
-def get_random_pair_of_matching_colours
-  random_palette = [:complement, :split_complement, :tetrad, :analogous, :monochromatic].sample
+def random_pair_of_matching_colours
+  random_palette = [:complement, :split_complement, :tetrad, :triad, :analogous, :monochromatic].sample
   Chroma.paint(SecureRandom.hex(3)).palette.send(random_palette, as: :name).sample(2)
+end
+
+def generate_cover(path)
+  base_image = Magick::ImageList.new($source_path)
+  base_image.level_colors(*random_pair_of_matching_colours).write(path)
 end
 
 puts "Generating #{$number_of_covers} Foobar covers..."
 progress = ProgressBar.create(total: $number_of_covers)
+
 $number_of_covers.times do |index|
-  logo_colour, background_colour = *get_random_pair_of_matching_colours
-  image = Magick::ImageList.new($source_path)
-  logo_image = image.level_colors(background_colour, logo_colour)
-  logo_image.write("covers/foobar-cover-#{index}.png")
+  generate_cover("covers/foobar-cover-#{index}.png")
   progress.increment
 end
+
 puts "Done."
